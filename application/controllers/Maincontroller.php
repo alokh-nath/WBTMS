55b75529c3b6cd0ba2963118ae0f42c4aad57d0c
alokh-nath <alokhnath1@gmail.com>
2022-08-10 22:44:02 +0530

    First Commit

---
+++ application/controllers/Maincontroller.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Maincontroller extends CI_Controller {
+
+    public function __construct()
+    {
+        parent :: __construct();
+        $this->load->model('Mainmodel');
+    }
+
+	public function index()
+	{
+		$this->load->view('home');
+	}
+
+    public function about()
+    {
+        $this->load->view('about');
+    }
+}
---
+++ application/models/Mainmodel.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mainmodel extends CI_Model {
+
+}
